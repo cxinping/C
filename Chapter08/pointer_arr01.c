0660050cac1f1b816b30ac259f9c5bbc49a53f7b
cxinping <xpws2006@163.com>
2020-03-02 06:33:34 +0800

    指针运算符

---
+++ Chapter08/pointer_arr01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+const int MAX = 3;
+ 
+int main ()
+{
+   int  var[] = {10, 100, 200};
+   int  i, *ptr;
+ 
+   /* 指针中的数组地址 */
+   ptr = var;
+   for ( i = 0; i < MAX; i++)
+   {
+ 
+      printf("var[%d]的存储地址 = %p\n", i, ptr );
+      printf("var[%d]的存储值 = %d\n", i, *ptr );
+ 
+      /* 移动到下一个位置 */
+      ptr++;
+   }
+   return 0;
+}
