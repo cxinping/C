14437fd3e96ea4e042bf68f00261e8dafe57ec98
cxinping <xpws2006@163.com>
2020-03-02 02:35:23 +0800

    指针例子

--- Chapter08/pointer02.c
+++ Chapter08/pointer02.c
@@ -8,12 +8,8 @@ int main()
     p = &var;  /* 在指针变量中存储 var 的地址 */
 
     printf("变量var的地址是: %p\n", &var  );
-
-    /* 在指针变量中存储的地址 */
     printf("指针变量p存储的是: %p\n", p );
     printf("指针变量p的地址是: %p\n", &p );
-
-    /* 使用指针访问值 */
     printf("指针*p访问值是: %d\n", *p );
 
     return 0;
