5450f55b9fa50b8065f517a971575db1581dc7c3
cxinping <xpws2006@163.com>
2020-03-03 06:13:11 +0800

    用指针直接指向字符串

---
+++ Chapter08/pointer_arr062.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>   //为了使用 strlen
+
+int main ()
+{
+    // 定义一个字符串，用指针s指向这个字符串
+    char *s = "xp";
+
+    // 使用strlen函数测量字符串长度
+    int len = strlen(s);
+
+    printf("字符串长度：%d", len);
+
+    return 0;
+}
