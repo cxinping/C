19f27b8e11817d30380a8db1c4b49378cc207ed3
cxinping <xpws2006@163.com>
2020-03-03 05:57:05 +0800

    字符串

--- Chapter08/pointer_arr05.c
+++ Chapter08/pointer_arr05.c
@@ -2,7 +2,19 @@
  
 int main ()
 {
+    // 定义一个int类型的数组
+    int a[4] = {1, 2, 3, 4};
 
+    // 定义一个int类型的指针，并指向数组的第0个元素
+    int *p = a;
+
+    int i;
+    for (i = 0; i < 4; i++) {
+        // 利用指针运算符*取出数组元素的值
+        int value = *(p+i);
+        
+        printf("a[%d] = %d \n", i, value);
+    }
 
     return 0;
 }
--- Chapter08/pointer_arr06.c
+++ Chapter08/pointer_arr06.c
@@ -2,12 +2,18 @@
  
 int main ()
 {
-    char *str = "hello";
+    // 定义一个指针p
+    char *p;
 
-    for (int i = 0; i < 5; i++)
-   {
-      printf("character = %c\n", *str++ );
-   }
+    // 定义一个数组s存放字符串
+    char s[] = "xp";
+
+    // 指针p指向字符串的首字符'x'
+    p = s; // 或者 p = &s[0];
+
+    for (; *p != '\0'; p++) {
+        printf("character = %c \n", *p);
+    }
 
     return 0;
 }
