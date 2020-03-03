74bf9a064e174a7d8ef461928be166d08316cf3a
cxinping <xpws2006@163.com>
2020-03-03 10:54:48 +0800

    枚举例子

--- Chapter09/enum03.c
+++ Chapter09/enum03.c
@@ -2,15 +2,15 @@
  
 enum Season 
 { 
-    Spring = 1,Summer , Autumn , Winter 
+    Spring ,Summer , Autumn , Winter 
 }season;
  
 int main()
 {
-
-
-
-
-
+    int a = 2;
+    enum Season season;
+    season = ( enum Season ) a;  // 强制类型转换
+    //season = 2;        // 直接给枚举变量赋值是错误的
+    printf("season=%d",season);
     return 0;
 }
---
+++ Chapter09/enum04.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    enum color { red=1, green, blue };
+    enum color favorite_color;
+  
+    printf("请输入你喜欢的颜色: (1. red, 2. green, 3. blue): ");
+    scanf("%d", &favorite_color);
+ 
+    /* 输出结果 */
+    switch (favorite_color)
+    {
+        case red:
+            printf("你喜欢的颜色是红色");
+            break;
+        case green:
+            printf("你喜欢的颜色是绿色");
+            break;
+        case blue:
+            printf("你喜欢的颜色是蓝色");
+            break;
+        default:
+            printf("你没有选择你喜欢的颜色");
+    }
+ 
+    return 0;
+}
