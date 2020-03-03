42623f630602b082cdc0b30e2a9810d30cbcd0a7
cxinping <xpws2006@163.com>
2020-03-04 05:43:34 +0800

    枚举例子

--- Chapter09/struct01.c
+++ Chapter09/struct01.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
- 
+#include <string.h>
+
 int main()
 {
+    struct Person
+    {
+        int index;
+        char name[30];
+        short age ;
+    } person;
+ 
+    person.index = 1;
+    strcpy(person.name, "信平");
+    person.age = 28;
 
-
+    /* 输出 person 信息 */
+    printf( "person.index = %d\n", person.index);
+    printf( "person.name = %s\n", person.name);
+    printf( "person.age = %d\n", person.age);
 
     return 0;
 }
