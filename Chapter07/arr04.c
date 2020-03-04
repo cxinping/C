ce66f7d803c8896b66b50b1e893c5005cd8958bc
cxinping <xpws2006@163.com>
2020-03-04 22:12:48 +0800

    数组

--- Chapter07/arr04.c
+++ Chapter07/arr04.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 // b是change函数的形参(形式参数)
-void change(int b) {
-    b = 9;
+void change(int b[]) {
+    b[0] = 9;
 }
 
 int main ()
@@ -11,7 +11,7 @@ int main ()
     a[0] = 10;
     
     printf("函数调用前的a[0]：%d\n", a[0]);
-    change(a[0]); // a[0]是change函数的实参(实际参数)
+    change(a); // a是change函数的实参(实际参数)
     printf("函数调用后的a[0]：%d", a[0]);
 
     return 0;
