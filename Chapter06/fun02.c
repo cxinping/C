68e685c5ebb887ef874af19e5bb29d5737ea860d
cxinping <xpws2006@163.com>
2020-03-05 02:28:54 +0800

    函数例子

--- Chapter06/fun02.c
+++ Chapter06/fun02.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-
-
-int main ()
-{
-    
-
-    return 0;
-}
+// 只是做个函数声明，并不用实现
+ int sum(int a, int b);
+ 
+ int main()
+ {
+     int c = sum(1, 4);
+     return 0;
+ }
+ 
+ // 函数的定义(实现)
+ int sum(int a, int b) {
+     return a + b;
+ }
