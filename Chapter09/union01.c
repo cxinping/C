395be444dd87c04a6f6496bbb177baeedda295cc
cxinping <xpws2006@163.com>
2020-03-04 16:40:12 +0800

    共用体

--- Chapter09/union01.c
+++ Chapter09/union01.c
@@ -1,7 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
  
-int main()
+union Data
 {
-
-    return 0;
+   int i;
+   float f;
+   char  str[20];
+};
+ 
+int main( )
+{
+   union Data data;        
+ 
+   data.i = 10;
+   printf( "data.i : %d\n", data.i);
+   
+   data.f = 220.5;
+   printf( "data.f : %f\n", data.f);
+   
+   strcpy( data.str, "C Programming");
+   printf( "data.str : %s\n", data.str);
+ 
+   return 0;
 }
