dd42b14583a67f317c25a40b3d4a41205fbbe222
cxinping <xpws2006@163.com>
2020-03-04 19:52:03 +0800

    数组

---
+++ Chapter07/arr01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main ()
+{
+    int a[3]; 
+    printf("a[0] address = %x \n", &a[0]);
+    printf("a[1] address = %x\n" , &a[1]);
+    printf("a[2] address = %x\n" , &a[2]);
+    return 0;
+}
