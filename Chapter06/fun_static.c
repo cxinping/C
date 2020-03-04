513f091fd4ec6c52ef4b0259d80f6c9837e816f9
cxinping <xpws2006@163.com>
2020-03-05 01:12:21 +0800

    函数例子

--- Chapter06/fun_static.c
+++ Chapter06/fun_static.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int add(int x){
+    static int n = 0;
+    n = n + x;
+    return n;
+}
+
 int main ()
 {
+    int i,j,sum;
+    i = 5;
+    for(j=1;j<=i;j++){
+        sum = add(j);
+        printf("j=%d, sum=%d\n",j , sum);
+    }
+
 
     return 0;
 }
