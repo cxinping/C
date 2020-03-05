941a722985c3ccd75c678169ec5a6a75772266f4
cxinping <xpws2006@163.com>
2020-03-05 22:49:16 +0800

    循环

---
+++ Chapter05/example01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main ()
+{
+   /* 局部变量定义 */
+   int i, j;
+   
+   for(i=2; i<10; i++) {
+      for(j=2; j <= (i/j); j++)
+        if(!(i%j)) 
+            break; // 如果找到，则不是质数
+      if(j > (i/j)) 
+        printf("%d 是质数\n", i);
+   }
+ 
+   return 0;
+}
