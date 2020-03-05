c5580da5d4fb4bc0ad62d3c36ec5ee33dc269e27
cxinping <xpws2006@163.com>
2020-03-06 00:42:25 +0800

    数据类型

---
+++ Chapter02/test01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>       
+
+int main()
+{
+    printf("整型常量 %d\n" , 100);   
+    printf("字符常量 %c\n" , 'a');   
+    printf("浮点型常量 %f\n" , 3.1415);   
+    printf("字符串常量 %s\n" , "hello world");   
+
+    return 0;
+}
