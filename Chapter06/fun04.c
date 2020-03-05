039ab2646238cf4e201752961662493b8a9bdb93
cxinping <xpws2006@163.com>
2020-03-05 19:06:12 +0800

    函数例子

--- Chapter06/fun04.c
+++ Chapter06/fun04.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+void swap(int v1, int v2) {
+    printf("更换前：v1=%d, v2=%d\n", v1, v2);
+    
+    // 定义一个中间变量
+    int temp;
+    
+    // 交换v1和v2的值
+    temp = v1;
+    v1 = v2;
+    v2 = temp;
+    
+    printf("更换后：v1=%d, v2=%d\n", v1, v2);
+}
 
- int main()
- {
-   
-     return 0;
- } 
+int main()
+{
+    int a = 10, b = 9;
+    printf("更换前：a=%d, b=%d\n", a, b);
+    
+    swap(a, b);
+    
+    printf("更换后：a=%d, b=%d", a, b);
+    return 0;
+}
